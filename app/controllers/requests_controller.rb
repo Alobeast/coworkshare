4bf107854724f3e249c2f3cd6ee0e6e4d5d87bfa
Alois Beard <alois.beard@gmail.com>
2019-05-24 16:33:35 +0200

    added simple_form_for and bootstrap gems & created basic new and create methods

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,17 @@
 class RequestsController < ApplicationController
   def new
+    @request = Request.new
+  end
 
+  def create
+    @request = Request.new(request_params)
+    @request.save
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:first_name, :last_name, :email,
+                                    :phone_number, :about)
   end
 end
