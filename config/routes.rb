abd3c6e437531c7456334b59c1401ff8dc140ca5
Alois Beard <alois.beard@gmail.com>
2019-05-24 14:02:53 +0200

    created pages controller with home & thanks, + requests controller with new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/thanks'
+  resources :requests, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
