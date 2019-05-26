0bc0ae58c052c1807c17ee29861fcfe57fd1c34d
Alois Beard <alois.beard@gmail.com>
2019-05-26 22:24:07 +0200

    added scopes & .accept! method

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,6 @@
 class Request < ApplicationRecord
+  attr_accessor :status
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
@@ -7,6 +9,17 @@ class Request < ApplicationRecord
   validates :about, presence: true, length: { minimum: 100 }
 
   after_create :send_confirmation_email
+
+  scope :unconfirmed, -> { where(status: 'unconfirmed') }
+  scope :confirmed, -> { where(status: 'confirmed') }
+  scope :accepted, -> { where(status: 'accepted') }
+  scope :expired, -> { where(status: 'expired') }
+
+  def accept!
+    unless status != "confirmed"
+      update(status: "accepted")
+    end
+  end
 
   private
 
