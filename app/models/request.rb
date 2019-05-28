beb9833f158b8d5799d2448a2817902a3f1a4fcf
Alois Beard <alois.beard@gmail.com>
2019-05-28 19:40:24 +0200

    updated optional regexes

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,10 +4,10 @@ class Request < ApplicationRecord
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :email, presence: true, uniqueness: true
-  # , format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/}
+  # , format: { with: /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/}
   ## optional regex
   validates :phone_number, presence: true, uniqueness: true, length: { minimum: 10 }
-  # , format: { with: /((\+)33|0|0033)[1-9](\d{2}){4}/} ## optional regex
+  # , format: { with: /\A((\+)33|0|0033)[1-9](\d{2}){4}\z/} ## optional regex
   validates :about, presence: true, length: { minimum: 100 }
   after_create :send_confirmation_email
 
