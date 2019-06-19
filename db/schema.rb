4d64bce427423a0a04adfb90efc8b2c67d492ec5
Alois Beard <alois.beard@gmail.com>
2019-06-19 09:32:22 +0200

    created Service model, controller + migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_160851) do
+ActiveRecord::Schema.define(version: 2019_06_18_125156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,14 @@ ActiveRecord::Schema.define(version: 2019_06_17_160851) do
     t.datetime "updated_at", null: false
     t.integer "size"
     t.integer "price"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "type"
+    t.integer "quantity_day"
+    t.integer "price_unit_day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
